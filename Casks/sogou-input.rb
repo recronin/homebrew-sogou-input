d74816e32f4d5f98869601fb00a9c274293df450
GitHub Action <action@github.com>
2025-05-23 05:51:20 +0000

    Update Sogou Input to v4.7.0.5698

--- Casks/sogou-input.rb
+++ Casks/sogou-input.rb
@@ -2,8 +2,7 @@ cask "sogou-input" do
   version "4.7.0.5698"
   sha256 "12569c55927582274997c811e8953613e25a00c694480be4f2ef3af71bbf2731"
 
-  url "https://github.com/recronin/homebrew-sogou-input/releases/download/v#{version}/sogou_input_v#{version}.zip",
-      verified: "github.com/recronin/homebrew-sogou-input/"
+  url "https://github.com/recronin/homebrew-sogou-input/releases/download/v#{version}/sogou_input_v#{version}.zip"
   name "Sogou Input Method"
   desc "Chinese input method"
   homepage "https://pinyin.sogou.com/mac/"
@@ -16,7 +15,6 @@ cask "sogou-input" do
   auto_updates true
   depends_on macos: ">= :mojave"
   depends_on arch: [:x86_64, :arm64]
-
   stage_only true
 
   postflight do
@@ -24,12 +22,7 @@ cask "sogou-input" do
     if app_path.nil?
       puts "No Sogou app found in #{staged_path}"
     else
-      system_command "/usr/bin/open",
-                     args:         [app_path],
-                     print_stderr: false,
-                     print_stdout: false
+      system "/usr/bin/open", app_path
     end
-  rescue
-    nil
   end
 end
